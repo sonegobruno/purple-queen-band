c2700c5b9049142ce6cfe3eec371bb5c3fd339f9
sonegobruno <sonegobruno@gmail.com>
2023-09-19 19:38:31 -0300

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { Anchor } from '@/components'
+import { House } from '@phosphor-icons/react'
+
+export default function NotFound() {
+  return (
+    <main className="w-full h-screen bg-dark flex flex-col items-center justify-center">
+      <strong className="text-light text-lg text-center">
+        Não encontramos a página solicitada,
+        <br />
+        por favor acesse abaixo para retornar a página principal!
+      </strong>
+      <Anchor href="./" className="text-light w-[280px] mt-8">
+        <House size={18} />
+        Início
+      </Anchor>
+    </main>
+  )
+}
