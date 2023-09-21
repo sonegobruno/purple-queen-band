4bdda6047a3c6adf57befb4fe3b48620391f0493
sonegobruno <sonegobruno@gmail.com>
2023-09-21 10:14:47 -0300

    feat: change title

--- src/templates/Home/components/Hero.tsx
+++ src/templates/Home/components/Hero.tsx
@@ -6,15 +6,15 @@ export function Hero() {
   return (
     <section className="flex w-full h-[calc(100vh-80px)] container mx-auto">
       <div className="w-full h-full flex flex-col items-center justify-center p-4 md:p-8">
-        <h1 className="text-2xl md:text-4xl font-bold leading-10 mt-4 text-center">
-          Conheça <br />
-          nossa banda
+        <h1 className="text-2xl md:text-3xl font-bold leading-10 text-center">
+          Banda Purple Queen
         </h1>
-        <p className="text-sm  md:text-lg  mt-3 text-center">
-          Uma experiência Rock n&apos; Roll única
+        <p className="text-xs md:text-md mt-2 text-center">
+          Banda de Rock localizada no sul de Minas Gerais <br />
+          que irá te trazer uma experiência Rock n&apos; Roll única.
         </p>
         <Anchor href="contact" className="mt-4">
-          Nos chame para seu evento <Guitar weight="fill" />
+          Agende o seu show <Guitar weight="fill" />
         </Anchor>
       </div>
     </section>
